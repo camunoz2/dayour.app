862e1745cfd33d9e512992eddf94addd3d96ceed
cristian <camunoz2@gmail.com>
2022-12-10 19:04:43 -0300

    3d

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,25 @@
+import { Canvas, useLoader, useFrame, PrimitiveProps } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import type { NextPage } from 'next'
+import { PresentationControls } from '@react-three/drei'
 import { signIn } from 'next-auth/react'
-
 import Head from 'next/head'
 import Layout from '../components/Layout'
+import { Suspense, useRef } from 'react'
+
+function CustomScene() {
+  useFrame(({ clock }) => {
+    const a = clock.getElapsedTime()
+  })
+  const model = useLoader(GLTFLoader, '/model.gltf')
+  const myModel = useRef()
+
+  return (
+    <Suspense fallback={null}>
+      <primitive object={model.scene} />
+    </Suspense>
+  )
+}
 
 const Home: NextPage = () => {
   return (
@@ -16,27 +33,51 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <h1 className="text-9xl font-black">DAYOUR</h1>
-        <p className="text-2xl">
-          Unleash the power of &apos;Productivity&apos;
-        </p>
-        <div className="mt-12">
-          <p className="text-bold text-xl">
-            Log in with{' '}
-            <span
-              onClick={() => signIn('github', { callbackUrl: '/dashboard' })}
-              className="bg-red-900 text-orange-100 px-4 py-2 rounded-md cursor-pointer hover:bg-red-500"
-            >
-              Github
-            </span>{' '}
-            or{' '}
-            <span
-              onClick={() => signIn('google', { callbackUrl: '/dashboard' })}
-              className="bg-red-900 text-orange-100 px-4 py-2 rounded-md cursor-pointer hover:bg-red-500"
-            >
-              Google
-            </span>
-          </p>
+        <div className="flex">
+          <div>
+            <h1 className="text-8xl md:text-[170px] leading-tight font-black">
+              DAYOUR
+            </h1>
+            <p className="text-xl md:text-3xl">
+              Unleash the power of &apos;Productivity&apos;
+            </p>
+            <div className="mt-12">
+              <p className="text-bold text-xl">
+                Log in with{' '}
+                <span
+                  onClick={() =>
+                    signIn('github', { callbackUrl: '/dashboard' })
+                  }
+                  className="bg-red-900 text-orange-100 px-4 py-2 rounded-md cursor-pointer hover:bg-red-500"
+                >
+                  Github
+                </span>{' '}
+                or{' '}
+                <span
+                  onClick={() =>
+                    signIn('google', { callbackUrl: '/dashboard' })
+                  }
+                  className="bg-red-900 text-orange-100 px-4 py-2 rounded-md cursor-pointer hover:bg-red-500"
+                >
+                  Google
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="absolute -translate-x-1/2 left-1/2 lg:left-1/2 lg:translate-x-0 top-16 lg:top-1/4  h-60 w-60 lg:h-[450px] lg:w-[450px]   mix-blend-luminosity">
+            <Canvas dpr={[1, 2]} camera={{ fov: 30, position: [0, 0, 8] }}>
+              <ambientLight />
+              <PresentationControls
+                global
+                zoom={0.8}
+                rotation={[0, -Math.PI / 4, -Math.PI / 4]}
+                polar={[0, Math.PI / 4]}
+                azimuth={[-Math.PI / 4, Math.PI / 4]}
+              >
+                <CustomScene />
+              </PresentationControls>
+            </Canvas>
+          </div>
         </div>
       </Layout>
     </>
